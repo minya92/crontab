856404f3849883a944ff4ea1b166167cd28cfa91
Mikhail Lapshin (work) <lapshin.michail@gmail.com>
2015-10-05 17:06:54 +0300

    add install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
 #!/bin/sh 
-sudo apt-get install screen nano cron
+sudo apt-get install screen nano cron git
+sudo mkdir /var/crontab
+sudo mkdir /var/crontab/bin
+sudo mkdir /var/crontab/logs
+sudo mkdir /var/crontab/apps
+cd /var/crontab/bin
+wget http://lapshina.net/crontab/run
+wget http://lapshina.net/crontab/testApp-start
+cd ../apps
+git clone https://github.com/minya92/crontab.git
+rename ./crontab ./testApp
+cd ./testApp
+git fetch
+git pull
+echo "VISIT http://localhost:3333/"
+node server.js
